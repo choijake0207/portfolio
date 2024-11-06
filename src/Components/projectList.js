bf3461e9b886045ac0327499e1acfb12eaf09fcd
choijake0207 <jake.ch0712@gmail.com>
2024-11-05 16:27:44 -0800

    fix image import paths

--- src/Components/projectList.js
+++ src/Components/projectList.js
@@ -1,3 +1,8 @@
+import EchoDemo from "../Assets/EchoDemo.png"
+import HabitDemo from "../Assets/HabitPic.png"
+import Lift4DummiesDemo from "../Assets/Lift4Dummies.png"
+import TaskerDemo from "../Assets/TaskerPic.png"
+
 export const projects = [
     {
         id: 0,
@@ -6,7 +11,8 @@ export const projects = [
             "React.js", "Express.js", "Node.js", "Rest APIs", "JWT", "MySQL"
         ],
         description: "A full-stack social media application built similarly to the style of Twitter. Users are able to post content as well as liking, commenting, and saving other's posts.",
-        liveStatus: false
+        liveStatus: false,
+        imageURL: EchoDemo
     },
     {
         id: 1,
@@ -15,7 +21,8 @@ export const projects = [
             "React.js", "Express.js", "Node.js", "Rest APIs", "MySQL"
         ],
         description: "A full-stack habit management platform allowing users to create, pause, and reset habits and duration-based goals. Users are also able to view all data including number of resets, number of completed goals, and more.",
-        liveStatus: false
+        liveStatus: false,
+        imageURL: HabitDemo
     },
     {
         id: 2,
@@ -24,7 +31,8 @@ export const projects = [
             "React.js, @dnd-kit"
         ],
         description: "A client-side personal KanBan board allowing users to create boards, customize columns, and drag-drop items.",
-        liveStatus: true
+        liveStatus: true,
+        imageURL: TaskerDemo
 
     },
     {
@@ -32,7 +40,8 @@ export const projects = [
         projectName: "Lift-4-Dummies",
         skills: ["Vanilla JS"],
         description: "A client-side project designed to allow users to learn about fitness techniques in an interactive method through the use of clickable SVGs.",
-        liveStatus: true
+        liveStatus: true,
+        imageURL: Lift4DummiesDemo
     }
    
 ]
