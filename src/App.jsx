724bc5067c28a5dd87cb4d6b5d1a6292fcbd9537
choijake0207 <jake.ch0712@gmail.com>
2024-11-05 16:31:56 -0800

    create header w links/import in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./App.css"
+import Gallery from './Components/Gallery'
+import AboutMe from './Components/AboutMe'
+import Contact from './Components/Contact'
+import Skills from './Components/Skills'
+import {GithubLogo, List} from "phosphor-react"
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="portfolio">
+      <header className="header">
+        <a>
+          <GithubLogo/>
+          choijake0207
+        </a>
+        <List/>
+      </header>
+      <AboutMe/>
+      <Skills/>
+      <Gallery/>
+      <Contact/>
     </div>
   )
 }
