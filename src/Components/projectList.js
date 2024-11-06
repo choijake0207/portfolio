d84c5c9d77d906e04c28a223f060f022f82230f9
choijake0207 <jake.ch0712@gmail.com>
2024-11-05 16:10:14 -0800

    add live status

--- src/Components/projectList.js
+++ src/Components/projectList.js
@@ -1,11 +1,12 @@
 export const projects = [
     {
         id: 0,
-        projectName: "Echo (v 1.0)",
+        projectName: "Echo-v1",
         skills: [
             "React.js", "Express.js", "Node.js", "Rest APIs", "JWT", "MySQL"
         ],
-        description: "A full-stack social media application built similarly to the style of Twitter. Users are able to post content as well as liking, commenting, and saving other's posts."
+        description: "A full-stack social media application built similarly to the style of Twitter. Users are able to post content as well as liking, commenting, and saving other's posts.",
+        liveStatus: false
     },
     {
         id: 1,
@@ -13,7 +14,8 @@ export const projects = [
         skills: [
             "React.js", "Express.js", "Node.js", "Rest APIs", "MySQL"
         ],
-        description: "A full-stack habit management platform allowing users to create, pause, and reset habits and duration-based goals. Users are also able to view all data including number of resets, number of completed goals, and more."
+        description: "A full-stack habit management platform allowing users to create, pause, and reset habits and duration-based goals. Users are also able to view all data including number of resets, number of completed goals, and more.",
+        liveStatus: false
     },
     {
         id: 2,
@@ -21,14 +23,16 @@ export const projects = [
         skills: [
             "React.js, @dnd-kit"
         ],
-        description: "A client-side personal KanBan board allowing users to create boards, customize columns, and drag-drop items."
+        description: "A client-side personal KanBan board allowing users to create boards, customize columns, and drag-drop items.",
+        liveStatus: true
 
     },
     {
         id: 3,
-        projectName: "Lift 4 Dummies",
+        projectName: "Lift-4-Dummies",
         skills: ["Vanilla JS"],
-        description: "A client-side project designed to allow users to learn about fitness techniques in an interactive method through the use of clickable SVGs."
+        description: "A client-side project designed to allow users to learn about fitness techniques in an interactive method through the use of clickable SVGs.",
+        liveStatus: true
     }
    
 ]
