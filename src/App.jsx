cc6b4a529e88597594099a2ed02b03cbf99aaf5a
choijake0207 <jake.ch0712@gmail.com>
2024-11-06 17:49:34 -0800

    add: nav bar with visibility toggle / nav and social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import Gallery from './Components/Gallery'
 import AboutMe from './Components/AboutMe'
@@ -7,15 +7,38 @@ import Skills from './Components/Skills'
 import {GithubLogo, List} from "phosphor-react"
 
 export default function App() {
+  const [sideNavView, setSideNavView] = useState(false)
   return (
     <div className="portfolio">
+
+
       <header className="header">
-        <a>
-          <GithubLogo/>
+        <a id="github-link" href="https://github.com/choijake0207">
+          <GithubLogo size="1.5em"/>
           choijake0207
         </a>
-        <List/>
+        <button 
+            className="nav-toggle"
+            onClick={() => setSideNavView(!sideNavView)}
+        >
+          <List size="2em"/>
+        </button>
       </header>
+
+
+      <nav className={`side-nav ${sideNavView ? "visible" : ""}`}>
+        <div className="nav-links">
+          <a>About Me</a>
+          <a>Skills</a>
+          <a>Projects</a>
+          <a>Contact</a>
+        </div>
+        <div className="social-links">
+          <a>Download Resume</a>
+        </div>
+      </nav>
+
+
       <AboutMe/>
       <Skills/>
       <Gallery/>
